a88a841e2c0899feb396dc3fc6a15b5d576e2d5f
chaicopadillag <xt9738@rimac.com.pe>
2025-02-05 11:31:55 -0500

    Add documentation for Abstract Factory pattern in index.ts

--- src/creational/abstract-factory/index.ts
+++ src/creational/abstract-factory/index.ts
@@ -4,6 +4,19 @@ import { GasVehicleFactory } from './factories/gas-vehicle.factory.ts';
 import { HealthyFoodRestaurantFactory } from './factories/healthy-food-restaurant.factory.ts';
 import { RestaurantService } from './services/restaurant.service.ts';
 import { VehicleService } from './services/vehicle.service.ts';
+/**
+ * ! Abstract Factory:
+ * Es un patrón de diseño que permite crear familias de objetos relacionados
+ * sin especificar sus clases concretas.
+ *
+ * En lugar de crear objetos individuales directamente,
+ * creamos fábricas que producen un conjunto de objetos relacionados.
+ *
+ * * Es útil cuando necesitas crear objetos que son parte de una familia
+ * * y quieres asegurarte de que estos objetos se complementen entre sí.
+ *
+ * https://refactoring.guru/es/design-patterns/abstract-factory
+ */
 
 export const mainAbstractFactory = () => {
   console.log('===Pedido de Fast Food===');
