211b92b7c8502d83f63daf33fa998f339cdd964b
chaicopadillag <xt9738@rimac.com.pe>
2025-02-04 21:09:50 -0500

    Add Builder and Factory Method pattern documentation in respective files

--- src/creational/builder/builders/computer.builder.ts
+++ src/creational/builder/builders/computer.builder.ts
@@ -1,18 +1,3 @@
-/**
- * ! Patrón Builder:
- * Es un patrón de diseño creacional que nos permite construir objetos complejos
- * paso a paso.
- *
- * El patrón nos permite producir distintos tipos y representaciones
- * de un objeto empleando el mismo código de construcción.
- *
- * * Es útil cuando necesitamos construir un objeto complejo con muchas partes
- * * y queremos que el proceso de construcción sea independiente de las partes
- * * que lo componen.
- *
- * https://refactoring.guru/es/design-patterns/builder
- */
-
 import { ComputerBuilderI } from '../interfaces/computer-builder.interface.ts';
 import { ComputerI } from '../interfaces/computer.interface.ts';
 import { Computer } from '../models/computer.ts';
