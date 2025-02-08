4c683489e2909a99480a12748bbd655a48957d4b
chaicopadillag <xt9738@rimac.com.pe>
2025-02-07 19:04:04 -0500

    feat: override notify and setChannel methods in AlertNotification and ReminderNotification classes

--- src/design-patterns/structural/bridge/services/alert-notification.service.ts
+++ src/design-patterns/structural/bridge/services/alert-notification.service.ts
@@ -3,12 +3,12 @@ import { Notification } from '../abstractions/notification.ts';
 import { NotificationChannelI } from '../interfaces/notification-chanel.interface.ts';
 
 export class AlertNotification extends Notification {
-  notify(message: string): void {
+  override notify(message: string): void {
     console.log('\n%cNotificación de Alerta:', COLORS.red);
     this.channel.send(message);
   }
 
-  setChannel(channel: NotificationChannelI): void {
+  override setChannel(channel: NotificationChannelI): void {
     this.channel = channel;
   }
 }
--- src/design-patterns/structural/bridge/services/reminder-notification.service.ts
+++ src/design-patterns/structural/bridge/services/reminder-notification.service.ts
@@ -3,12 +3,12 @@ import { Notification } from '../abstractions/notification.ts';
 import { NotificationChannelI } from '../interfaces/notification-chanel.interface.ts';
 
 export class ReminderNotification extends Notification {
-  notify(message: string): void {
+  override notify(message: string): void {
     console.log('\n%cNotificación de Recordatorio:', COLORS.blue);
     this.channel.send(message);
   }
 
-  setChannel(channel: NotificationChannelI): void {
+  override setChannel(channel: NotificationChannelI): void {
     this.channel = channel;
   }
 }
