392fe087358a93920435cac7b525f86715c47ff4
chaicopadillag <xt9738@rimac.com.pe>
2025-02-08 11:05:25 -0500

    feat: optimize BulletTypeFactory to use a key variable for bullet type retrieval

--- src/design-patterns/structural/flyweight/factories/bullet-type.factory.ts
+++ src/design-patterns/structural/flyweight/factories/bullet-type.factory.ts
@@ -5,10 +5,11 @@ export class BulletTypeFactory {
   private bulletTypes: Map<string, BulletType> = new Map();
 
   getBulletType(name: string, damage: number, color: string): BulletType {
-    if (!this.bulletTypes.has(name + '-' + damage + '-' + color)) {
-      console.log(`%cCreating new bullet type: ${name}-${damage}-${color}`, COLORS.red);
-      this.bulletTypes.set(name + '-' + damage + '-' + color, new BulletType(name, damage, color));
+    const key = `${name}-${damage}-${color}`;
+    if (!this.bulletTypes.has(key)) {
+      console.log(`%cCreating new bullet type: ${key}`, COLORS.red);
+      this.bulletTypes.set(key, new BulletType(name, damage, color));
     }
-    return this.bulletTypes.get(name + '-' + damage + '-' + color)!;
+    return this.bulletTypes.get(key)!;
   }
 }
