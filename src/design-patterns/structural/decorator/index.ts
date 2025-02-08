b2444144f1f8d6dbd31bc6620003af7af33a75ed
chaicopadillag <xt9738@rimac.com.pe>
2025-02-08 08:46:16 -0500

    docs: add documentation for Decorator pattern in index.ts

--- src/design-patterns/structural/decorator/index.ts
+++ src/design-patterns/structural/decorator/index.ts
@@ -1,5 +1,19 @@
 import { DecoratorService } from './services/decorator.service.ts';
 
+/**
+ * ! Patrón decorador
+ * Es un patrón de diseño estructural que permite añadir
+ * funcionalidades a objetos, colocando estos objetos dentro de
+ * objetos encapsuladores especiales que contienen estas funcionalidades.
+ *
+ * No confundirlo con los decoradores de TypeScript que son anotaciones.
+ *
+ * * Es útil cuando necesitas añadir funcionalidades a objetos
+ *  * de manera dinámica y flexible.
+ *
+ * https://refactoring.guru/es/design-patterns/decorator
+ */
+
 export const mainDecorator = () => {
   DecoratorService.run();
 };
